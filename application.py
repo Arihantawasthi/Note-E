4ea34e7eec50be7d77767c472f5b0d7e13330476
Arihantawasthi <awasthiarihant@gmail.com>
2018-09-22 23:03:11 +0530

    Added basic functionalities

---
+++ application.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template, session, request
+from flask_session import Session
+
+app = Flask(__name__)
+
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+notes = []
+
+@app.route('/', methods=["POST", "GET"])
+def index():
+    if session.get("notes") is None:
+        session["notes"] = []
+
+    if request.method == "POST":
+        note = request.form.get("notes")
+        session["notes"].append(note)
+
+    return render_template('index.html', notes=session["notes"])
